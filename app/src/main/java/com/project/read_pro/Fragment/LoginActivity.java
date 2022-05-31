2d3d74e5247deba95855baefce18c18fea86f29b
unknown <you@example.com>
2022-05-31 10:25:32 +0700

    Authentication

--- app/src/main/java/com/project/read_pro/Fragment/LoginActivity.java
+++ app/src/main/java/com/project/read_pro/Fragment/LoginActivity.java
@@ -2,29 +2,67 @@ package com.project.read_pro.Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.project.read_pro.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText editEmail,editPass;
+    View textSignup ;
+    View btnLogin ;
+    String email,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        View textSignup = findViewById(R.id.text_signUP);
+        textSignup  = findViewById(R.id.text_signUP);
+        btnLogin = findViewById(R.id.button_login);
+        editEmail = findViewById(R.id.edit_email);
+        editPass = findViewById(R.id.edit_pass);
+
+        btnLogin.setOnClickListener(v -> checkLogin());
 
         textSignup.setOnClickListener(
                 v -> openActivity2()
         );
     }
+
+    private void checkLogin() {
+        email = editEmail.getText().toString();
+        pass = editPass.getText().toString();
+        if(email.isEmpty()|| pass.isEmpty()){
+            FailedSent();
+        }else {
+            sentLogin();
+        }
+    }
+
+    private void FailedSent() {
+        Toast.makeText(this,"Email and password is required",Toast.LENGTH_LONG).show();
+    }
+    private void sentLogin() {
+
+        Toast.makeText(this,"Sent",Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
+        finish();
+    }
     public void openActivity2() {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
     }
+
+
+
 }
--- app/src/main/java/com/project/read_pro/MainActivity.java
+++ app/src/main/java/com/project/read_pro/MainActivity.java
@@ -2,7 +2,12 @@ package com.project.read_pro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.project.read_pro.Fragment.HomeFragment;
+import com.project.read_pro.Fragment.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        },1000);
+
     }
 }
